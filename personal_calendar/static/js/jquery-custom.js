25f0718655ab5df642df57436cbb3159aa4dc5c8
bilou <ga_bilou@hotmail.com>
2013-10-02 01:02:21 +0200

    ajax interface

--- personal_calendar/static/js/jquery-custom.js
+++ personal_calendar/static/js/jquery-custom.js
@@ -1,3 +1,50 @@
 jQuery(function($) {
-  var i = $('#id_date').datepicker();
+  // Customized accordion
+  $('#id_date').datepicker();
+  $('#content > ul > li > :not(h3)').hide();
+  $('.title').click(function(e) {
+    $(this).parent().parent().children(':not(h3)').slideToggle();
+    e.preventDefault();
+  });
+  //Ajax: add participants to an event
+  $('#participant_form').submit(function(e) {
+    $.ajax(
+      {
+        type: "POST",
+        data: $(this).serialize(),
+        url: "",
+        success: function(data) {
+          if(typeof(data) == 'string'){
+            $("#participants_form").html(data);
+          } else {
+            $('#participants').append(
+              "<div>"
+             + data.participant + " | "
+             + data.get_status_display + ""
+             + data.delete_form +
+              "</div>"
+              );
+          }
+        },
+      }
+      );
+      e.preventDefault();
+  });
+
+  //Ajax: remove participants from an event
+  $(document).on('click', '.delete', function(e) {
+    e.preventDefault();
+    var form = $(this);
+    $.ajax(
+    {
+      type: "POST",
+      data: $(this).serialize(),
+      url: $(this).attr('action'),
+      success: function(data) {
+        if(data == 'OK') {
+          form.parent(':not(#content)').remove();
+        }
+      }
+    });
+  });
 });
--- personal_calendar/views.py
+++ personal_calendar/views.py
@@ -1,7 +1,12 @@
 #-*-coding: utf-8-*-
-from django.shortcuts import render, HttpResponseRedirect
+import json
+
+from django.shortcuts import (render, HttpResponseRedirect,
+    render_to_response, HttpResponse)
 from django.contrib.auth.models import User
 from django.forms import HiddenInput
+from django.template.loader import render_to_string
+from django.template import RequestContext
 
 from forms import EventForm, Evenement_ParticipantForm
 from models import Evenement, Evenement_Participant
@@ -34,17 +39,36 @@ def details(request, id):
     form = Evenement_ParticipantForm(request.POST)
     if form.is_valid():
       form.save()
+      if request.is_ajax():
+        delete_form = render_to_string("personal_calendar/blocks/delete_form.html",
+                                       {
+                                         'delete_url': form.instance.delete_url(),
+                                       },
+                                       # Needed for the csrf_token
+                                       RequestContext(request)
+                                      )
+        data = {'participant': form.instance.participant.username,
+                'get_status_display': form.instance.get_status_display(),
+                'delete_form': delete_form}
+        return HttpResponse(json.dumps(data), mimetype="application/json")
+
       return HttpResponseRedirect('/agenda/%s/details/' % id)
   else:
     form = Evenement_ParticipantForm(initial={'evenement': event})
     participants = [user.pk for user in event.participants.all()]
     form.fields['participant'].queryset = User.objects.exclude(pk__in=participants)
     form.fields['evenement'].widget = HiddenInput()
+  if request.is_ajax():
+    return render_to_response('personal_calendar/blocks/participant_form.html',
+                              {
+                                'event': event,
+                                'form': form
+                              })
   return render(request, 'personal_calendar/event/details.html',
-      {
-        'event': event,
-        'form': form
-      })
+                {
+                  'event': event,
+                  'form': form
+                })
 
 def delete(request, id):
   if request.method == 'POST':
@@ -61,6 +85,8 @@ def delete_participant(request, id, participant):
         participant=participant,
         )
     to_delete.delete()
+    if request.is_ajax():
+      return HttpResponse('OK')
   return HttpResponseRedirect('/agenda/%s/details/' % id)
 
 def liste(request):
