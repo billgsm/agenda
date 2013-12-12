6f6d565562ec7690ee20e9ff62e4185b23416f78
bilou <ga_bilou@hotmail.com>
2013-12-12 16:57:57 +0100

    security

--- agenda/prod_settings.py
+++ agenda/prod_settings.py
@@ -1,3 +1,8 @@
+import os
+
+from credentials import *
+project_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 DEBUG = False
 DATABASES = {
     'default': {
@@ -12,6 +17,6 @@ DATABASES = {
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'ad.dydict.min@gmail.com'
-EMAIL_HOST_PASSWORD = 'testsendingmail'
+EMAIL_HOST_USER = login
+EMAIL_HOST_PASSWORD = password
 EMAIL_PORT = 587
